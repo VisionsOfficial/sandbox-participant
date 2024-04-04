82e270072de6a6d821e26f3e7511084ad011a796
Visions-Nicolas <nicolas@visionspol.eu>
2024-04-04 17:12:08 +0200

    feat: added html and PDI

--- src/routes/public/v1/index.ts
+++ src/routes/public/v1/index.ts
@@ -1,9 +1,14 @@
 import usersPublicRouter from "./users.public.router";
+import frontPublicRouter from "./front.public.router";
 
 const routers = [
     {
         prefix: "/users",
         router: usersPublicRouter,
+    },
+    {
+        prefix: "/",
+        router: frontPublicRouter,
     },
 ];
 
