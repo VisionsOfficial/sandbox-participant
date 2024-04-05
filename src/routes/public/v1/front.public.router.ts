4c0bf15b237a4d727fcd52a13a5bffb9f3473012
Visions-Nicolas <nicolas@visionspol.eu>
2024-04-05 10:42:58 +0200

    feat: added error.ejs

--- src/routes/public/v1/front.public.router.ts
+++ src/routes/public/v1/front.public.router.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import path from "path";
 import axios from "axios";
 
 const r: Router = Router();
 
 r.get("/", async (req, res) => {
     try {
-        console.log(req.headers);
-        console.log(req.hostname);
         const userId = "65646d4320ec42ff2e719706";
         const DSCAdminJWT =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlS2V5IjoiQTVkdzY5OHNuWGlKUlZmYjZjTTR1RDd3M2JDTWdhbllKZ2VIWmZERGNIcE41QnlESmJOUE1XQm50TktCYVhqTlJ6dVd6NzRRUDlHVU5ZWEdxR2plVWJNMzY3YUhaTnNaRlNKNCIsIm9yaWdpbiI6Imh0dHBzOi8vcHJvdmlkZXItYXBpLWE3MDc2NWEyMjBhNC5oZXJva3VhcHAuY29tIiwiaWF0IjoxNzEyMjQzMTA3NTg0fQ.uVY_D6L1jeighZgi024sUgyo3XvCj0q56YgXIb7HRaQ";
@@ -40,7 +37,9 @@ r.get("/", async (req, res) => {
             privacyNoticeEndpoint: `https://provider-data-connector-253244a6c16c.herokuapp.com/private/consent/${userId}/privacy-notices/65e9f407de244c2a18764332`,
         });
     } catch (e) {
-        console.error(e.message);
+        res.render("error", {
+            error: e,
+        });
     }
 });
 
