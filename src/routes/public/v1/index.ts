4ba394d042240d181905eb3b29b83fed5c5c6c40
Visions-Nicolas <nicolas@visionspol.eu>
2025-04-15 10:19:16 +0200

    feat: added lrc and de-identifier endpoint

--- src/routes/public/v1/index.ts
+++ src/routes/public/v1/index.ts
@@ -2,6 +2,8 @@ import usersPublicRouter from "./users.public.router";
 import consumerPublicRouter from "./consumer.public.router";
 import infrastructurePublicRouter from "./infrastructure.public.router";
 import prePublicRouter from "./pre.public.router";
+import lrcPublicRouter from "./lrc.public.router";
+import deIdentifierPublicRouter from "./deIdentifier.public.router";
 
 const routers = [
     {
@@ -19,6 +21,14 @@ const routers = [
     {
         prefix: "/pre",
         router: prePublicRouter,
+    },
+    {
+        prefix: "/lrc",
+        router: lrcPublicRouter,
+    },
+    {
+        prefix: "/de-identifier",
+        router: deIdentifierPublicRouter,
     },
 ];
 
