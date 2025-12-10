69061dfc39ad9aaadeffd4434c08f47a6cb14624
Visions-David <david@visionspol.eu>
2025-12-10 10:17:08 +0100

    feat : consume & store consumer new route implementation

--- src/routes/public/v1/consumer.public.router.ts
+++ src/routes/public/v1/consumer.public.router.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { consume } from "../../../controllers/public/v1/consumer.public.controller";
+import {
+    consume,
+    consumeAndStore,
+} from "../../../controllers/public/v1/consumer.public.controller";
 
 const r: Router = Router();
 
 r.post("/consume", consume);
+
+r.post("/consume/store", consumeAndStore);
 
 export default r;
