9cd4aa7bf536b24476fdc3e647b47626580e4872
Visions-David <david@visionspol.eu>
2025-12-10 10:17:35 +0100

    feat : consume & store consumer new route logic implementation

--- src/controllers/public/v1/consumer.public.controller.ts
+++ src/controllers/public/v1/consumer.public.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ConsumedData } from "../../../models";
 
 /**
  * Consumer the data
@@ -12,6 +13,32 @@ export const consume = async (
         return res.json({
             message: "Received Data.",
             dataReceived: req.body,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+/**
+ * Consumer the data and store it
+ */
+export const consumeAndStore = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const consumedData = new ConsumedData({
+            payload: req.body,
+            headers: req.headers,
+        });
+
+        await consumedData.save();
+
+        return res.json({
+            message: "Data received and stored.",
+            dataReceived: req.body,
+            storedId: consumedData._id,
         });
     } catch (err) {
         next(err);
